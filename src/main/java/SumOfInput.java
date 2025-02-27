eec2ff906191e66a34a15942f060e322aa9f3a6f
Kunle <kunle@Zipcoders-MacBook-Pro-5.local>
2025-02-27 14:40:59 -0500

    Got it started

---
+++ src/main/java/SumOfInput.java
@@ -0,0 +1,9 @@
+public class SumOfInput{
+	public static void main(String[] args){
+	
+		Scanner number = new Scanner(System.in);
+		
+		System.out.print("Please enter a number: ");
+		int n = number.nextLine();
+}
+}
