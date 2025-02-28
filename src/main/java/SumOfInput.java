e13cfd59fbc1484b11e28775abdbe8bf73bcd728
Kunle <kunle@Zipcoders-MacBook-Pro-5.local>
2025-02-28 13:33:39 -0500

    tracking time is finsihed

--- src/main/java/SumOfInput.java
+++ src/main/java/SumOfInput.java
@@ -7,16 +7,48 @@ public class SumOfInput{
 		System.out.println("Please enter a number: ");
 		int n = number.nextInt();
 
-		System.out.println(sumNumbers(n));
+		/* long startTime, endTime, elapsedTime;
+		startTime = System.currentTimeMillis();
+		int sumNum = sumNumbers(n);
+		endTime = System.currentTimeMillis();
+		elapsedTime = endTime - startTime; */
+
+		//System.out.println("sumNumbers resulted in: " + sumNum + " sumNumbers took " +elapsedTime + " ms");
+		System.out.println(" sumNumbers completes the task in " + sumNumbers(n) + " ms");
+		System.out.println("guassianWizardry completes the task in " + guassianWizardry(n) + " ms");
 
 
 	}
 	// Method to add numbers
-	public static int sumNumbers(int j){
-		int sum = 0;
+	public static long sumNumbers(int j){
+		int sumLoopMethod = 0;
+		long startTime, endTime, elapsedTime;
+
+		startTime = System.currentTimeMillis();
 		for(int i = 1; i<=j; i++){
-			sum = sum + i;
+			sumLoopMethod = sumLoopMethod + i;
 		}
-		return sum;
+		endTime = System.currentTimeMillis();
+		elapsedTime = endTime - startTime;
+
+		return elapsedTime;
+		//return sumLoopMethod;
+	}
+
+	// Guassian Wizardry
+	public static long guassianWizardry(int x){
+		int sumGuassianMethod = 0;
+		long startTime2, endTime2, elapsedTime2;
+
+		startTime2 = System.currentTimeMillis();
+		sumGuassianMethod = (x*(x+1))/2;
+		endTime2 = System.currentTimeMillis();
+
+		elapsedTime2 = endTime2 - startTime2;
+
+		return elapsedTime2;
+
+
+		//return sumGuassianMethod;
 	}
 }
