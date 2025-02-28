88f238105267189f1146dc92f5180120bfe6324b
Kunle <kunle@Zipcoders-MacBook-Pro-5.local>
2025-02-28 11:27:29 -0500

    sumNumbers works

--- src/main/java/SumOfInput.java
+++ src/main/java/SumOfInput.java
@@ -1,9 +1,22 @@
+import java.util.Scanner; // import the Scanner class
+
 public class SumOfInput{
 	public static void main(String[] args){
-	
+		// asking the user for number to sum up to
 		Scanner number = new Scanner(System.in);
-		
-		System.out.print("Please enter a number: ");
-		int n = number.nextLine();
-}
+		System.out.println("Please enter a number: ");
+		int n = number.nextInt();
+
+		System.out.println(sumNumbers(n));
+
+
+	}
+	// Method to add numbers
+	public static int sumNumbers(int j){
+		int sum = 0;
+		for(int i = 1; i<=j; i++){
+			sum = sum + i;
+		}
+		return sum;
+	}
 }
